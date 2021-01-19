287d0cc8719f7b35933e215d09d4bf158d61d0a2
JorbFreire <jorbfreire@ufrn.edu.br>
2021-01-18 22:45:43 -0300

    merge files

--- api/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const routes = express.Router();
-
-routes.get('/', (request, response) => {
-  return response.status(200).json({ response: "ok" })
-});
-
-module.exports = routes;
